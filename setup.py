a106f2f5d68cd4fe1d05704ed771664ad62fa4a2
Greg <gregory.rolwes@slu.edu>
2020-12-12 20:03:28 -0600

    Add Progress Bar
    - Progress object facilitates threading for Dashboard
    - Dashboard object passes metrics to ProgressBar and controls display
    - ProgressBar display progress bar and percentage of a specified size

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="ml_progress", # Replace with your own username
+    name="ml_progress",
     version="0.0.1",
     author="Greg",
     author_email="rolwesg@gmail.com",
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/gregrolwes/ml_progress",
     packages=setuptools.find_packages(),
+    package_dir={"": "src"},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
