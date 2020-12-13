0a052002260d8363e68c63ac8c07be14bc005ea8
Greg <gregory.rolwes@slu.edu>
2020-12-12 20:24:01 -0600

    Updated setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="ml_progress",
-    version="0.0.1",
+    version="0.0.2",
     author="Greg",
     author_email="rolwesg@gmail.com",
     description="A small example package",
